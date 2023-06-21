88e51243ccb797c6da6d6a271c2d523171ef1c41
Himanshu Gupta <himanshugupta543210@gmail.com>
2023-06-21 17:41:42 +0530

    --

--- HR1.cpp
+++ HR1.cpp
@@ -1,19 +1,24 @@
-#include<stdio.h>
+#include<stdio.h>// write two functions to print hello and bye
+
 // function declaration/ prototype (1)
 void printHello();
-
+void printBye();
 
 int main(){
     printHello();//funtion call (3)
+    printBye();
+    
 
 return 0;
 
 }
 // funtion definition (2)
 void printHello(){
-
     printf("Hello \n");
-}
+ }
+ void printBye(){
+    printf("Bye");
+ }
 
 
 
