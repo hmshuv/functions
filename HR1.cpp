d928fe339049614c4e75bddc1131677f3eb9712b
Himanshu Gupta <himanshugupta543210@gmail.com>
2023-06-21 16:48:41 +0530

    function syntax

---
+++ HR1.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// function declaration/ prototype (1)
+void printHello();
+
+
+int main(){
+    printHello();//funtion call (3)
+
+return 0;
+
+}
+// funtion definition (2)
+void printHello(){
+
+    printf("Hello \n");
+}
+
+
+
